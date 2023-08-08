e5dc6f9b25f04d86d0d0cebcc959e6aab74ff77e
laurourbano <lauro@outlook.com.br>
2023-08-08 20:46:59 -0300

    configurando o ambiente e dependencias

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,5 @@ import { server } from './server/Server';
 const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}.`);
+    console.log(`Server is listening on port ${ PORT || 3000 }.`);
 });
